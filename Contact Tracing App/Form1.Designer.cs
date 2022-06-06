eda20d355eb2e19b5c8ab17a4229629113bc650e
Patrick Joseph Magbuhos <patrickjosephmagbuhos@gmail.com>
2022-06-07 01:33:13 +0800

    Other Text Box Added

--- Contact Tracing App/Form1.Designer.cs
+++ Contact Tracing App/Form1.Designer.cs
@@ -30,24 +30,238 @@ namespace Contact_Tracing_App
         private void InitializeComponent()
         {
             this.txtbxFN = new System.Windows.Forms.TextBox();
+            this.fn = new System.Windows.Forms.Label();
+            this.txtbxMN = new System.Windows.Forms.TextBox();
+            this.mn = new System.Windows.Forms.Label();
+            this.txtbxLN = new System.Windows.Forms.TextBox();
+            this.ln = new System.Windows.Forms.Label();
+            this.personalinfo = new System.Windows.Forms.Label();
+            this.strt = new System.Windows.Forms.TextBox();
+            this.txtbxSTRT = new System.Windows.Forms.Label();
+            this.txtbxBRNGY = new System.Windows.Forms.TextBox();
+            this.brgy = new System.Windows.Forms.Label();
+            this.txtbxPRV = new System.Windows.Forms.TextBox();
+            this.prv = new System.Windows.Forms.Label();
+            this.txtbxAG = new System.Windows.Forms.TextBox();
+            this.ag = new System.Windows.Forms.Label();
+            this.txtbxDT = new System.Windows.Forms.TextBox();
+            this.dt = new System.Windows.Forms.Label();
+            this.txtbxTP = new System.Windows.Forms.TextBox();
+            this.tp = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // txtbxFN
             // 
-            this.txtbxFN.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtbxFN.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtbxFN.ForeColor = System.Drawing.SystemColors.ScrollBar;
             this.txtbxFN.Location = new System.Drawing.Point(37, 76);
             this.txtbxFN.Name = "txtbxFN";
-            this.txtbxFN.Size = new System.Drawing.Size(208, 26);
+            this.txtbxFN.Size = new System.Drawing.Size(208, 25);
             this.txtbxFN.TabIndex = 0;
-            this.txtbxFN.Text = "--First Name--";
+            this.txtbxFN.Text = "--Ex: Juan--";
             this.txtbxFN.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // fn
+            // 
+            this.fn.AutoSize = true;
+            this.fn.Location = new System.Drawing.Point(89, 104);
+            this.fn.Name = "fn";
+            this.fn.Size = new System.Drawing.Size(72, 13);
+            this.fn.TabIndex = 1;
+            this.fn.Text = "FIRST NAME";
+            // 
+            // txtbxMN
+            // 
+            this.txtbxMN.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtbxMN.ForeColor = System.Drawing.SystemColors.ScrollBar;
+            this.txtbxMN.Location = new System.Drawing.Point(277, 76);
+            this.txtbxMN.Name = "txtbxMN";
+            this.txtbxMN.Size = new System.Drawing.Size(208, 25);
+            this.txtbxMN.TabIndex = 2;
+            this.txtbxMN.Text = "--Ex: Raymundo--";
+            this.txtbxMN.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // mn
+            // 
+            this.mn.AutoSize = true;
+            this.mn.Location = new System.Drawing.Point(342, 105);
+            this.mn.Name = "mn";
+            this.mn.Size = new System.Drawing.Size(82, 13);
+            this.mn.TabIndex = 3;
+            this.mn.Text = "MIDDLE NAME";
+            // 
+            // txtbxLN
+            // 
+            this.txtbxLN.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtbxLN.ForeColor = System.Drawing.SystemColors.ScrollBar;
+            this.txtbxLN.Location = new System.Drawing.Point(520, 76);
+            this.txtbxLN.Name = "txtbxLN";
+            this.txtbxLN.Size = new System.Drawing.Size(208, 25);
+            this.txtbxLN.TabIndex = 4;
+            this.txtbxLN.Text = "--Ex: Dela Cruz--";
+            this.txtbxLN.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // ln
+            // 
+            this.ln.AutoSize = true;
+            this.ln.Location = new System.Drawing.Point(581, 105);
+            this.ln.Name = "ln";
+            this.ln.Size = new System.Drawing.Size(82, 13);
+            this.ln.TabIndex = 5;
+            this.ln.Text = "MIDDLE NAME";
+            // 
+            // personalinfo
+            // 
+            this.personalinfo.AutoSize = true;
+            this.personalinfo.Location = new System.Drawing.Point(305, 48);
+            this.personalinfo.Name = "personalinfo";
+            this.personalinfo.Size = new System.Drawing.Size(149, 13);
+            this.personalinfo.TabIndex = 6;
+            this.personalinfo.Text = "-PERSONAL INFORMATION-";
+            // 
+            // strt
+            // 
+            this.strt.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.strt.ForeColor = System.Drawing.SystemColors.ScrollBar;
+            this.strt.Location = new System.Drawing.Point(37, 131);
+            this.strt.Name = "strt";
+            this.strt.Size = new System.Drawing.Size(242, 25);
+            this.strt.TabIndex = 7;
+            this.strt.Text = "--Ex: B1 L3 Harurut Street--";
+            this.strt.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtbxSTRT
+            // 
+            this.txtbxSTRT.AutoSize = true;
+            this.txtbxSTRT.Location = new System.Drawing.Point(89, 160);
+            this.txtbxSTRT.Name = "txtbxSTRT";
+            this.txtbxSTRT.Size = new System.Drawing.Size(105, 13);
+            this.txtbxSTRT.TabIndex = 8;
+            this.txtbxSTRT.Text = "STREET ADDRESS";
+            // 
+            // txtbxBRNGY
+            // 
+            this.txtbxBRNGY.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtbxBRNGY.ForeColor = System.Drawing.SystemColors.ScrollBar;
+            this.txtbxBRNGY.Location = new System.Drawing.Point(285, 131);
+            this.txtbxBRNGY.Name = "txtbxBRNGY";
+            this.txtbxBRNGY.Size = new System.Drawing.Size(187, 25);
+            this.txtbxBRNGY.TabIndex = 9;
+            this.txtbxBRNGY.Text = "--Ex: Barangay Anonas--";
+            this.txtbxBRNGY.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // brgy
+            // 
+            this.brgy.AutoSize = true;
+            this.brgy.Location = new System.Drawing.Point(358, 159);
+            this.brgy.Name = "brgy";
+            this.brgy.Size = new System.Drawing.Size(66, 13);
+            this.brgy.TabIndex = 10;
+            this.brgy.Text = "BARANGAY";
+            // 
+            // txtbxPRV
+            // 
+            this.txtbxPRV.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtbxPRV.ForeColor = System.Drawing.SystemColors.ScrollBar;
+            this.txtbxPRV.Location = new System.Drawing.Point(478, 131);
+            this.txtbxPRV.Name = "txtbxPRV";
+            this.txtbxPRV.Size = new System.Drawing.Size(250, 25);
+            this.txtbxPRV.TabIndex = 11;
+            this.txtbxPRV.Text = "--Ex: Metro Manila, Quezon City--";
+            this.txtbxPRV.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // prv
+            // 
+            this.prv.AutoSize = true;
+            this.prv.Location = new System.Drawing.Point(534, 160);
+            this.prv.Name = "prv";
+            this.prv.Size = new System.Drawing.Size(166, 13);
+            this.prv.TabIndex = 12;
+            this.prv.Text = "PROVINCE AND MUNICIPALITY";
+            // 
+            // txtbxAG
+            // 
+            this.txtbxAG.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtbxAG.ForeColor = System.Drawing.SystemColors.ScrollBar;
+            this.txtbxAG.Location = new System.Drawing.Point(37, 186);
+            this.txtbxAG.Name = "txtbxAG";
+            this.txtbxAG.Size = new System.Drawing.Size(82, 25);
+            this.txtbxAG.TabIndex = 13;
+            this.txtbxAG.Text = "--Ex: 18 --";
+            this.txtbxAG.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // ag
+            // 
+            this.ag.AutoSize = true;
+            this.ag.Location = new System.Drawing.Point(61, 214);
+            this.ag.Name = "ag";
+            this.ag.Size = new System.Drawing.Size(29, 13);
+            this.ag.TabIndex = 14;
+            this.ag.Text = "AGE";
+            // 
+            // txtbxDT
+            // 
+            this.txtbxDT.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtbxDT.ForeColor = System.Drawing.SystemColors.ScrollBar;
+            this.txtbxDT.Location = new System.Drawing.Point(140, 186);
+            this.txtbxDT.Name = "txtbxDT";
+            this.txtbxDT.Size = new System.Drawing.Size(139, 25);
+            this.txtbxDT.TabIndex = 15;
+            this.txtbxDT.Text = "--Ex: 06/09/2022--";
+            this.txtbxDT.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // dt
+            // 
+            this.dt.AutoSize = true;
+            this.dt.Location = new System.Drawing.Point(192, 214);
+            this.dt.Name = "dt";
+            this.dt.Size = new System.Drawing.Size(36, 13);
+            this.dt.TabIndex = 16;
+            this.dt.Text = "DATE";
+            // 
+            // txtbxTP
+            // 
+            this.txtbxTP.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtbxTP.ForeColor = System.Drawing.SystemColors.ScrollBar;
+            this.txtbxTP.Location = new System.Drawing.Point(37, 242);
+            this.txtbxTP.Name = "txtbxTP";
+            this.txtbxTP.Size = new System.Drawing.Size(101, 25);
+            this.txtbxTP.TabIndex = 17;
+            this.txtbxTP.Text = "--Ex: 36.5°C--";
+            this.txtbxTP.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // tp
+            // 
+            this.tp.AutoSize = true;
+            this.tp.Location = new System.Drawing.Point(34, 270);
+            this.tp.Name = "tp";
+            this.tp.Size = new System.Drawing.Size(89, 13);
+            this.tp.TabIndex = 18;
+            this.tp.Text = "TEMPERATURE";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(765, 450);
+            this.ClientSize = new System.Drawing.Size(765, 480);
+            this.Controls.Add(this.tp);
+            this.Controls.Add(this.txtbxTP);
+            this.Controls.Add(this.dt);
+            this.Controls.Add(this.txtbxDT);
+            this.Controls.Add(this.ag);
+            this.Controls.Add(this.txtbxAG);
+            this.Controls.Add(this.prv);
+            this.Controls.Add(this.txtbxPRV);
+            this.Controls.Add(this.brgy);
+            this.Controls.Add(this.txtbxBRNGY);
+            this.Controls.Add(this.txtbxSTRT);
+            this.Controls.Add(this.strt);
+            this.Controls.Add(this.personalinfo);
+            this.Controls.Add(this.ln);
+            this.Controls.Add(this.txtbxLN);
+            this.Controls.Add(this.mn);
+            this.Controls.Add(this.txtbxMN);
+            this.Controls.Add(this.fn);
             this.Controls.Add(this.txtbxFN);
             this.Name = "Form1";
             this.ResumeLayout(false);
@@ -58,6 +272,24 @@ namespace Contact_Tracing_App
         #endregion
 
         private System.Windows.Forms.TextBox txtbxFN;
+        private System.Windows.Forms.Label fn;
+        private System.Windows.Forms.TextBox txtbxMN;
+        private System.Windows.Forms.Label mn;
+        private System.Windows.Forms.TextBox txtbxLN;
+        private System.Windows.Forms.Label ln;
+        private System.Windows.Forms.Label personalinfo;
+        private System.Windows.Forms.TextBox strt;
+        private System.Windows.Forms.Label txtbxSTRT;
+        private System.Windows.Forms.TextBox txtbxBRNGY;
+        private System.Windows.Forms.Label brgy;
+        private System.Windows.Forms.TextBox txtbxPRV;
+        private System.Windows.Forms.Label prv;
+        private System.Windows.Forms.TextBox txtbxAG;
+        private System.Windows.Forms.Label ag;
+        private System.Windows.Forms.TextBox txtbxDT;
+        private System.Windows.Forms.Label dt;
+        private System.Windows.Forms.TextBox txtbxTP;
+        private System.Windows.Forms.Label tp;
     }
 }
 
